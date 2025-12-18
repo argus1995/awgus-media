2156260fe3fcabfff5738ba1de3c0ec8d643f38c
argus1995 <ariefagus1995@gmail.com>
2025-12-18 21:40:59 +0700

    Initial commit

---
+++ src/conts.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "Awgus Media";
+export const SITE_DESCRIPTION =
+  "Blog IT tentang Infrastruktur IT (Hardware & Networking), Digital Operation, dan Software & Scripts dengan penjelasan sederhana dan solusi praktis.";
